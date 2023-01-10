c0d2cc804a3cd887f85d5928fbae421c591e151c
kondasMajid <kondasmajid@gmai.com>
2023-01-10 12:35:05 +0000

    Form form uni pages to render external forms

---
+++ src/component/NewExpense/ExpenseForm.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+const ExpenseForm = () =>{
+ return <form>
+     <div className="new-expense__controls">
+         <div className="new-expense__control"></div>
+     </div>
+ </form>
+}
+
+export default ExpenseForm;
+
---
+++ src/component/NewExpense/NewExpenses.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './NewEspense.css';
+
+const NewExpenses = () => {
+    return <div>
+        <form action=""></form>
+    </div>
+};
+
+export default NewExpenses;
