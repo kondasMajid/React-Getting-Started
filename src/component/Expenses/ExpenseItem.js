11b2d7464d5fe0aca3e72864236611f20479f345
kondasMajid <kondasmajid@gmai.com>
2023-01-10 11:10:41 +0000

    Added sates to pass event to next page

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,17 +1,30 @@
 import ExpenseDate from "./ExpenseDate";
-import React from "react";
+import React, {useState} from "react";
 import './ExpenseItem.css'
 import Card from "../UI/Card";
+import {click} from "@testing-library/user-event/dist/click";
 export const ExpenseItem = (props) => {
+   const [title, seTitle] = useState(props.title)
+    const [amt, Amt] =useState(props.amount);
+    // let title = props.title;
+    const clickHandler = () => {
+        seTitle('Updated');
+        console.log(title);
+        Amt('200')
+    }
 
 
+    // const amount = () => {
+    //    Amt('200')
+    // }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title} </h2>
-                <div className="expense-item__price">$ {props.amount}</div>
+                <h2> --  {title} </h2>
+                <div className="expense-item__price">$ {amt}</div>
             </div>
+            <button onClick={clickHandler}>Click to Edit</button>
         </Card>
     )
 }
